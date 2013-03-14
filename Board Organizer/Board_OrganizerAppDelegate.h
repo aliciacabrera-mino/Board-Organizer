5fca3eaf066eccb7d2815a70d69a68e82050ad2e
AlexTong <alexander.tong@globalonlineacademy.org>
2013-03-13 20:36:01 -0700

    Initial Commit
    Empty Application Starting Point

---
+++ Board Organizer/Board_OrganizerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Board_OrganizerAppDelegate.h
+//  Board Organizer
+//
+//  Created by Alex on 3/13/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Board_OrganizerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Board Organizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Board Organizer
+//
+//  Created by Alex on 3/13/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Board_OrganizerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Board_OrganizerAppDelegate class]));
+    }
+}
